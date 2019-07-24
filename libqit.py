6938342794acd6b43650554867e08f35c48cca3f
Leo Qiao <leo.qiao@getnexar.com>
2019-07-24 21:06:14 +0300

    complete cat-file, checkout, hash-object, init, log, ls-tree, rev-parse, show-ref (bare minimum)

--- libqit.py
+++ libqit.py
@@ -202,9 +202,75 @@ def object_read(repo, sha):
             raise Exception("Unknown type {} for object {}".format(fmt.decode("ascii"), sha))
 
         return c(repo, raw[y+1:])
-    
+
 def object_find(repo, name, fmt=None, follow=True):
-    return name
+    sha = object_resolve(repo, name)
+
+    if not sha:
+        raise Exception("No such reference {0}.".format(name))
+    
+    if len(sha) > 1:
+        raise Exception("Ambiguous referene {0}: Candidates are:\n - {1}".format(name, "\n - ".join(sha)))
+
+    sha = sha[0]
+
+    if not fmt:
+        return sha
+
+    while True:
+        obj = object_read(repo, sha)
+
+        if obj.fmt == fmt:
+            return sha
+
+        if not follow:
+            return None
+
+        # follow tags
+        if obj.fmt == b"tag":
+            sha = obj.kvlm[b"object"].decode("ascii")
+        elif obj.fmt == b"commit" and fmt == b"tree":
+            sha = obj.kvlm[b"tree"].decode("ascii")
+        else:
+            return None
+    
+
+def object_resolve(repo, name):
+    """resolve name to an object hash in repo.
+    - HEAD literal
+    - short and long hashes
+    - tags
+    - branches
+    - remote branches"""
+    
+    candidates = list()
+    hashRE = re.compile(r"^[0-9A-Fa-f]{1,16}$")
+    smallHashRE = re.compile(r"^[0-9A-Fa-f]{1,16}$")
+
+    # empty string?
+    if not name.strip():
+        return None
+
+    # HEAD
+    if name == "HEAD":
+        return [ ref_resolve(repo, "HEAD") ]
+
+    if hashRE.match(name):
+        if len(name) == 40:
+            # complete hash
+            return [ name.lower() ]
+        elif len(name) >= 4:
+            # minimal length for hash is 4
+            name = name.lower()
+            prefix = name[0:2]
+            path = repo_dir(repo, "objects", prefix, mkdir=False)
+            if path:
+                rem = name[2:]
+                for f in os.listdir(path):
+                    if f.startswith(rem):
+                        candidates.append(prefix + f)
+
+    return candidates
 
 def object_write(obj, actually_write=True):
     # serialize object data
@@ -291,7 +357,7 @@ def object_hash(fd, fmt, repo=None):
 
     return object_write(obj, actually_write=bool(repo))
 
-# commit
+# commit object
 def kvlm_parse(raw, start=0, dct=None):
     if not dct:
         dct = collections.OrderedDict()
@@ -397,3 +463,203 @@ def log_graphviz(repo, sha, seen):
         p = p.decode("ascii")
         print("c_{0} -> c_{1};".format(sha, p))
         log_graphviz(repo, p, seen)
+
+class GitTreeLeaf(object):
+    def __init__(self, mode, path, sha):
+        self.mode = mode
+        self.path = path
+        self.sha = sha
+
+def tree_parse_one(raw, start=0):
+    # find the space terminator of the mode
+    x = raw.find(b" ", start)
+    assert (x - start == 5) or (x - start == 6)
+
+    # read the mode
+    mode = raw[start:x]
+
+    # find the NULL terminator of the path
+    y = raw.find(b"\x00", x)
+    path = raw[x+1:y]
+
+    # read the SHA and convert to hex string
+    # note: remove the '0x' at the beginning of hex()
+    sha = hex(
+            int.from_bytes(
+                raw[y+1:y+21], "big"))[2:]
+
+    return y+21, GitTreeLeaf(mode, path, sha)
+
+def tree_parse(raw):
+    pos = 0
+    max = len(raw)
+    ret = list()
+    while pos < max:
+        pos, data = tree_parse_one(raw, pos)
+        ret.append(data)
+
+    return ret
+
+def tree_serialize(obj):
+    ret = b""
+    for i in obj.items:
+        ret += i.mode
+        ret += b" "
+        ret += i.path
+        ret += b"\x00"
+        sha += int(i.sha, 16)
+        ret += sha.to_bytes(20, byteorder="big")
+
+    return ret
+
+class GitTree(GitObject):
+    fmt = b"tree"
+
+    def serialize(self):
+        return tree.serialize(self)
+
+    def deserialize(self, data):
+        self.items = tree_parse(data)
+
+# ls-tree
+argsp = argsubparsers.add_parser("ls-tree", help="pretty-print a tree object.")
+argsp.add_argument("object", help="the object to show.")
+
+def cmd_ls_tree(args):
+    repo = repo_find()
+    obj = object_read(repo, object_find(repo, args.object, fmt=b"tree"))
+
+    for item in obj.items:
+        print("{0} {1} {2}\t{3}".format(
+            "0" * (6 - len(item.mode)) + item.mode.decode("ascii"),
+            object_read(repo, item.sha).fmt.decode("ascii"),
+            item.sha,
+            item.path.decode("ascii")))
+
+# checkout
+argsp = argsubparsers.add_parser("checkout", help="checkout a commit inside of a directory.")
+argsp.add_argument("commit",
+                   help="the commit or tree to checkout.")
+argsp.add_argument("path",
+                   help="the EMPTY directory to checkout on.")
+
+def cmd_checkout(args):
+    repo = repo_find()
+
+    obj = object_read(repo, object_find(repo, args.commit))
+
+    # if the object is a commit, get its tree
+    if obj.fmt == b"commit":
+        obj = object_read(repo, obj.kvlm[b"tree"].decode("ascii"))
+
+    # verify that path is an empty directory
+    if os.path.exists(args.path):
+        if not os.path.isdir(args.path):
+            raise Exception("Not a directory {0}!".format(args.path))
+        if os.listdir(args.path):
+            raise Exception("Not empty {0}!".format(args.path))
+    else:
+        os.makedirs(args.path)
+
+    tree_checkout(repo, obj, os.path.realpath(args.path).encode())
+
+def tree_checkout(repo, tree, path):
+    for item in tree.items:
+        obj = object_read(repo, item.sha)
+        dest = os.path.join(path, item.path)
+
+        if obj.fmt == b"tree":
+            os.mkdir(dest)
+            tree_checkout(repo, obj, dest)
+        elif obj.fmt == b"blob":
+            with open(dest, "wb") as f:
+                f.write(obj.blobdata)
+
+def ref_resolve(repo, ref):
+    with open(repo_file(repo, ref), "r") as fp:
+        # discard the trailing newline
+        data = fp.read()[:-1]
+    if data.startswith("ref: "):
+        return ref_resolve(repo, data[5:])
+    else:
+        return data
+
+def ref_list(repo, path=None):
+    if not path:
+        path = repo_dir(repo, "refs")
+    ret = collections.OrderedDict()
+    # sort refs
+    for f in sorted(os.listdir(path)):
+        can = os.path.join(path,f)
+        if os.path.isdir(can):
+            ref[f] = ref_list(repo, can)
+        else:
+            ret[f] = ref_resolve(repo, can)
+
+    return ret
+
+# show-refs
+argsp = argsubparsers.add_parser("show-ref", help="list references.")
+
+def cmd_show_ref(args):
+    repo = repo_find()
+    refs = ref_list(repo)
+    show_ref(repo, refs, prefix="refs")
+
+def show_ref(repo, refs, with_hash=True, prefix=""):
+    for k, v in refs.items():
+        if type(v) == str:
+            print("{0}{1}{2}".format(
+                v + " " if with_hash else "",
+                prefix + "/" if prefix else "",
+                k))
+        else:
+            show_ref(repo, v, with_hash=with_hash, prefix="{0}{1}{2}".format(prefix, "/" if prefix else "", k))
+
+# tags
+class GitTag(GitCommit):
+    fmt = b"tag"
+
+argsp = argsubparsers.add_parser("tag", help="list and create tags.")
+argsp.add_argument("-a",
+                   action="store_true",
+                   dest="create_tag_object",
+                   help="whether to create a tag object")
+argsp.add_argument("name",
+                   nargs="?",
+                   help="the new tag's name")
+argsp.add_argument("object",
+                   default="HEAD",
+                   nargs="?",
+                   help="the object the new tag will point to")
+
+def cmd_tag(args):
+    repo = repo_find()
+
+    if args.name:
+        tag_create(args.name,
+                   args.object,
+                   type="object" if args.create_tag_object else "ref")
+    else:
+        refs = ref_list(repo)
+        show_ref(repo, refs["tags"], with_hash=False)
+
+# rev-parse
+argsp = argsubparsers.add_parser("rev-parse",
+                                 help="parse revision (or other objects) identifiers")
+argsp.add_argument("--qit-type",
+                   metavar="type",
+                   dest="type",
+                   choices=["blob", "commit", "tag", "tree"],
+                   default=None,
+                   help="specify the expected type")
+argsp.add_argument("name",
+                   help="the name to parse")
+
+def cmd_rev_parse(args):
+    if args.type:
+        fmt = args.type.encode()
+
+    repo = repo_find()
+
+    print(object_find(repo, args.name, args.type, follow=True))
